d5843e722d03d36f84ad9a29a5117759f3044ea8
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2018-03-02 19:02:44 +0530

    Added 11 Completed TODO's
    Completed 11 TODO's in Lesson04a-Starting-New-Activities\T04a.01-Exercise-AddNewActivit

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -23,6 +23,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.net.URI;
 import android.widget.Toast;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
