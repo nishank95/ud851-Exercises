18fbc9b6945c6a38ac90e864e78cdb14a39647d3
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2018-03-22 03:21:35 +0530

    Addded 3 Completed TODO's
    Completed 3 TODO's in Lesson06-Visualizer-Preferences\T06.10-Exercise-EditTextPreferenceConstraints

--- Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -29,7 +29,7 @@ import android.widget.Toast;
 
 // TODO (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener,Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -52,6 +52,9 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             }
         }
         // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+        Preference pref= findPreference(getString(R.string.pref_size_key));
+        pref.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -92,6 +95,27 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
     // to a float; if it cannot, show a helpful error message and return false. If it can be converted
     // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
     // an error message and return false. If it is a valid number, return true.
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Toast error =Toast.makeText(getContext(),"Please enter a size between 0.1 & 3",Toast.LENGTH_LONG);
+
+        String sizeKey=getString(R.string.pref_size_key);
+        if(preference.getKey().equals(sizeKey)){
+            String stringSize=(String)newValue;
+            try{
+                float size=Float.parseFloat(stringSize);
+                if(size>3 || size <=0){
+                    error.show();
+                    return false;
+                }
+            }
+            catch (NumberFormatException e){
+                error.show();
+                return false;
+            }
+        }
+        return true;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -106,4 +130,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         getPreferenceScreen().getSharedPreferences()
                 .unregisterOnSharedPreferenceChangeListener(this);
     }
+
+
 }
