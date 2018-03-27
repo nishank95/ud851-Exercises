adfcdab8647a46294ba27b501eae6350adf5573e
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2018-03-27 13:57:43 +0530

    Added 20 completed TODO's
    Completed 20 TODO's inLesson07-Waitlist\T07.05-Exercise-AddGuests

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -58,7 +58,13 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
     }
 
     // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
-
+    public void swapCursor(Cursor newCursor){
+    if(mCursor!=null)
+        mCursor.close();
+    mCursor =newCursor;
+    if(newCursor != null)
+        this.notifyDataSetChanged();
+    }
     // TODO (16) Inside, check if the current cursor is not null, and close it if so
 
     // TODO (17) Update the local mCursor to be equal to  newCursor
